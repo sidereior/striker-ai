7cbbb668c07a1e26865df170acbe909b3f9d182d
sidereior <alexanderlnanda@gmail.com>
2023-11-07 01:40:45 -0500

    pre_preocessing and actions

--- pre_processing.py
+++ pre_processing.py
@@ -1,23 +1,12 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
 
-# Set up Chrome options for headless execution
-options = Options()
-options.add_argument("--headless")  # Runs Chrome in headless mode.
-options.add_argument("--no-sandbox")  # Bypass OS security model
-options.add_argument("--disable-gpu")  # applicable to windows os only
-options.add_argument("start-maximized")  # open Browser in maximized mode
-options.add_argument("disable-infobars")  # disabling infobars
-options.add_argument("--disable-extensions")  # disabling extensions
-options.add_argument("--disable-dev-shm-usage")  # overcome limited resource problems
-
 # Start the Selenium WebDriver
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome()
 
 # Function to save the current page as HTML
 def save_html(division, week):
