e3e638d3bc9f4eac1bd917644fa9a5c3814eec38
sidereior <alexanderlnanda@gmail.com>
2023-12-14 15:11:14 -0500

    percentages working

--- wyscout-scraper/pdfjson.py
+++ wyscout-scraper/pdfjson.py
@@ -1,6 +1,8 @@
+
 import pdfplumber
 import json
 import re
+import datetime
 
 pdf_path = 'test.pdf'
 
@@ -13,11 +15,9 @@ def extract_and_format_data(page_number):
     formatted_data = []
 
     def process_stat(stat):
-        # Check if the input is alphanumeric, ends with a single quote, or is a single dash
-        if re.match('^[a-zA-Z0-9]*$', stat) or stat.endswith("'") or stat == '-':
+        # Modified regex to include special characters
+        if stat.endswith("'") or stat == '-' or '/' not in stat:
             return stat
-        elif '/' not in stat:  # If stat is a number
-            return float(stat)
         else:  # If stat is a fraction and a percentage
             fraction, percent = stat.split('%')[0].split('/')
             numerator = int(fraction)
@@ -29,32 +29,50 @@ def extract_and_format_data(page_number):
                     return f'{numerator}/{denominator}'
                 elif numerator/int(denominator) == int(percent)/100:  # If the fraction matches the percentage
                     return f'{numerator}/{denominator}'
-            return 'ERROR'  # If the fraction doesn't match the percentage and it's over 100%
+            return 'ERROR:'  # If the fraction doesn't match the percentage and it's over 100%
+    
+    # Generate a timestamped filename
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    output_filename = f'output{timestamp}.txt'
 
-    with pdfplumber.open(pdf_path) as pdf:
+    with pdfplumber.open(pdf_path) as pdf, open(output_filename, 'w') as output_file:
         page = pdf.pages[page_number]
         text = page.extract_text()
         if text:
             lines = text.split('\n')
             start_processing = False
             for line in lines:
+                # Write each line to the file
+                output_file.write(line + '\n')
+
                 if "Player" in line:
                     start_processing = True
                     continue
                 if start_processing and line.strip():
-                    segments = line.split(maxsplit=2)
-                    if len(segments) < 3:
-                        continue
+                    segments = line.split()
+                    player_number = segments[0]
+                    indexToGoTo = -1
 
-                    player_number, player_name, stats_str = segments
-                    stats = [process_stat(stat) for stat in stats_str.split()]
+                    for(segment_index, segment) in enumerate(segments):
+                        if "'" in segment:
+                            indexToGoTo = segment_index-1
+                            break
+                            
+                    player_name_full = ""
+                    for i in range(1, indexToGoTo+1, 1):
+                       player_name_full += segments[i] + " " 
+                    player_name_full = player_name_full.strip() 
+                    #player_name = segments[1:indexToGoTo+1]
+                    stats_str = segments[indexToGoTo+1:]
+                    for i in stats_str:
+                        print(i)
+                    stats = [process_stat(stat) for stat in stats_str]
 
                     formatted_stats = dict(zip(data_format, stats))
-                    formatted_player_data = {"Player Number": player_number, "Player Name": player_name}
+                    formatted_player_data = {"Player Number": player_number, "Player Name": player_name_full}
                     formatted_player_data.update(formatted_stats)
 
                     formatted_data.append(formatted_player_data)
-
     return formatted_data
 
 # Extracting and formatting data from page 7
