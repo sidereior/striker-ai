de8f677d10fcd76eb731f76809f908594976c7e4
sidereior <alexanderlnanda@gmail.com>
2024-04-04 19:29:43 -0400

    django finish

--- striker-ai/strikerdjango/strikerdjango/urls.py
+++ striker-ai/strikerdjango/strikerdjango/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from strikerdjango import crewai_endpoint
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('crewai-endpoint/', views.crewai_endpoint, name='crewai_endpoint'),
+    path('crewai-endpoint/', crewai_endpoint, name='crewai_endpoint'),
 ]
